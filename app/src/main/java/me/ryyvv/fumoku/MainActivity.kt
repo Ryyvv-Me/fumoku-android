2117f7f38efb96c7900be5b5d57f3d7482cd50fa
Ryan Suhartanto <ryan.repeats@gmail.com>
2023-04-29 13:34:58 +0700

    Hello, World!

---
+++ app/src/main/java/me/ryyvv/fumoku/MainActivity.kt
@@ -0,0 +1,27 @@
+package me.ryyvv.fumoku
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.ui.Modifier
+import me.ryyvv.fumoku.ui.theme.FumokuTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            FumokuTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize()
+                ) {
+                    Text(
+                        text = "Hello, World!"
+                    )
+                }
+            }
+        }
+    }
+}
