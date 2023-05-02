3e98230f33c951f9a19dfc6daa11d5f60af74f34
Ryan Suhartanto <ryan.repeats@gmail.com>
2023-05-02 17:15:09 +0700

    Initialize scaffold

---
+++ app/src/main/kotlin/me/ryyvv/fumoku/MainActivity.kt
@@ -0,0 +1,82 @@
+package me.ryyvv.fumoku
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.ExperimentalLayoutApi
+import androidx.compose.foundation.layout.consumeWindowInsets
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import me.ryyvv.fumoku.ui.theme.FumokuTheme
+
+@OptIn(
+    ExperimentalLayoutApi::class,
+    ExperimentalMaterial3Api::class,
+)
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            FumokuTheme {
+                Scaffold(
+                    topBar = { TopBar() },
+                    bottomBar = { BottomBar() },
+                ) { innerPadding ->
+                    LazyColumn(
+                        modifier = Modifier.consumeWindowInsets(innerPadding),
+                        contentPadding = innerPadding,
+                    ) {
+
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+@OptIn(ExperimentalMaterial3Api::class)
+fun TopBar() {
+    TopAppBar(
+        title = { Text(stringResource(id = R.string.app_name)) },
+    )
+}
+
+@Preview
+@Composable
+fun BottomBar() {
+    var selectedItem by remember { mutableStateOf(0) }
+    val items = listOf(
+        "Home",
+        "Cart",
+        "Settings",
+    )
+
+    NavigationBar {
+        items.forEachIndexed { index, item ->
+            NavigationBarItem(
+                selected = selectedItem == index,
+                onClick = { selectedItem = index },
+                icon = { Icon(Icons.Default.Favorite, contentDescription = item) },
+                label = { Text(text = item) },
+            )
+        }
+    }
+}
