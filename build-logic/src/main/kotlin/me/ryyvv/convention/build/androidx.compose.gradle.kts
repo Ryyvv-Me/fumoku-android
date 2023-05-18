fc86570dd0caf57ef0a4e5ef02f24010584e8e03
Ryan Suhartanto <ryan.repeats@gmail.com>
2023-05-18 19:21:20 +0700

    Make compose configuration common

--- build-logic/src/main/kotlin/me/ryyvv/convention/build/androidx.compose.gradle.kts
+++ build-logic/src/main/kotlin/me/ryyvv/convention/build/androidx.compose.gradle.kts
@@ -1,10 +1,9 @@
 package me.ryyvv.convention.build
 
+import gradle.kotlin.dsl.accessors._47545ee4044af277c92cdb30c1d58315.androidTestImplementation
+import gradle.kotlin.dsl.accessors._47545ee4044af277c92cdb30c1d58315.implementation
+import me.ryyvv.convention.build.dsl.android
 import me.ryyvv.convention.build.dsl.libs
-
-plugins {
-    com.android.application
-}
 
 android {
     buildFeatures {
