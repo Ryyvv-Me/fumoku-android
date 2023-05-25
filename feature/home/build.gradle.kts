32c4a018ff8348a640da5bdc6f44a178cb75e70c
Ryan Suhartanto <ryan.repeats@gmail.com>
2023-05-25 13:13:02 +0700

    Initialize :feature:home library

---
+++ feature/home/build.gradle.kts
@@ -0,0 +1,4 @@
+plugins {
+    alias(libs.plugins.android.library)
+    id("me.ryyvv.convention.build.android")
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -16,3 +16,5 @@ dependencyResolutionManagement {
 
 rootProject.name = "fumoku"
 include(":app")
+
+include(":feature:home")
