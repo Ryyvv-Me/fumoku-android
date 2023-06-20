66546a9feaf68976d552cb12529d358151a977db
Ryan Suhartanto <ryan.repeats@gmail.com>
2023-06-20 10:58:59 +0700

    Use bundles for the gradle convention dependencies

--- gradle/convention/build.gradle.kts
+++ gradle/convention/build.gradle.kts
@@ -24,6 +24,5 @@ dependencies {
     // https://github.com/gradle/gradle/issues/15383
     implementation(files(libs::class.java.superclass.protectionDomain.codeSource.location))
 
-    implementation(libs.gradle.android)
-    implementation(libs.gradle.kotlin)
+    implementation(libs.bundles.gradle.convention)
 }
