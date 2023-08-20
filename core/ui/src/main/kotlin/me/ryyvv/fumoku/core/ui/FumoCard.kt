ae34c087f3463233ff0c0c0c0f18fd96785a747d
Ryan Suhartanto <ryan.repeats@gmail.com>
2023-08-20 20:33:44 +0700

    Basic layout for `FumoCard`

--- core/ui/src/main/kotlin/me/ryyvv/fumoku/core/ui/FumoCard.kt
+++ core/ui/src/main/kotlin/me/ryyvv/fumoku/core/ui/FumoCard.kt
@@ -1,12 +1,24 @@
 package me.ryyvv.fumoku.core.ui
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.AssistChip
 import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalInspectionMode
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
+import androidx.compose.ui.unit.dp
 import me.ryyvv.core.model.Fumo
 import me.ryyvv.fumoku.core.ui.theme.FumokuTheme
 
@@ -18,9 +30,40 @@ fun FumoCard(
     modifier: Modifier = Modifier
 ) {
     Card(
-        onClick = onClick
+        onClick = onClick,
+        modifier = modifier,
     ) {
+        Column {
+            // Image
+            if (fumo.image.isNotEmpty()) {
+                // TODO
+            }
 
+            Column(
+                modifier = Modifier.padding(16.dp)
+            ) {
+                // Character
+                Text(
+                    text = fumo.character.name,
+                    style = MaterialTheme.typography.headlineMedium,
+                )
+                // Name
+                Text(
+                    text = fumo.name,
+                    style = MaterialTheme.typography.titleLarge,
+                )
+
+                Spacer(modifier = Modifier.height(12.dp))
+
+                // Type
+                AssistChip(
+                    onClick = {},
+                    label = {
+                        Text(text = fumo.type.toString())
+                    },
+                )
+            }
+        }
     }
 }
 
