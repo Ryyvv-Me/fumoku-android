84f37dbc59566ea5685b74b2bb55440403308d0d
Ryan Suhartanto <ryan.repeats@gmail.com>
2023-08-20 19:37:03 +0700

    Use `Fumo`

--- core/ui/src/main/kotlin/me/ryyvv/fumoku/core/ui/FumoCard.kt
+++ core/ui/src/main/kotlin/me/ryyvv/fumoku/core/ui/FumoCard.kt
@@ -1,20 +1,30 @@
 package me.ryyvv.fumoku.core.ui
 
+import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import me.ryyvv.core.model.Fumo
 import me.ryyvv.fumoku.core.ui.theme.FumokuTheme
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun FumoCard(
-
+    fumo: Fumo,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier
 ) {
+    Card(
+        onClick = onClick
+    ) {
 
+    }
 }
 
 @Preview
 @Composable
 private fun FumoCardPreview() {
     FumokuTheme {
-        FumoCard()
     }
 }
