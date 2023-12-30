fb3071b74a73408c325874863e977e589ae53a22
Ryan Suhartanto <ryan.repeats@gmail.com>
2023-12-30 17:32:22 +0700

    Update `app` version to below v1 (development)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,7 +23,7 @@ android {
     defaultConfig {
         applicationId = namespace
         versionCode = 1
-        versionName = "1.0"
+        versionName = "0.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
